cfd705e4274573c0b616cd69351e155a2b3ed94a
D2-84110-Vishal-More <vishalm6566@gmail.com>
2024-03-06 20:12:32 +0530

    assignment-2 updated

--- Assign_2.c
+++ Assign_2.c
@@ -6,21 +6,34 @@
 
 # include<stdio.h>
 
-int factorial(int n)
+int factorialUsingRecurssion(int n)
 {
 	if(n==0)
 		return 1;
 	
-	return n * factorial(n-1);
+	return n * factorialUsingRecurssion(n-1);
+}
+
+int factorial(int n)
+{
+    int result = 1, i;
+ 
+    for (i = 2; i <= n; i++) {
+        result *= i;
+    }
+ 
+    return result;
 }
 
 int main()
 {
 	int n;
-	printf("program to calculate a Factorial of a number \nEnter Your Number : ");
+	printf("program to calculate a Factorial of a number with recursion \nEnter Your Number : ");
 	scanf("%d",&n);
 	
-	printf("Factorial of %d is %d",n, factorial(n));
+	printf("Factorial of %d by using recursion is %d \n",n, factorialUsingRecurssion(n));
+	
+	printf("Factorial of %d without using recursion is %d ",n, factorial(n));
 	
 	return 0;
 }
